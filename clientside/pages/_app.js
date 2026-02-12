680d6ec129f98283df4ca2693bf9fe980abef082
Natasha Nicole Ejercito <nnejercito@Natashas-MacBook-Pro.local>
2026-02-11 22:37:39 -0500

    _app.js updated

--- clientside/pages/_app.js
+++ clientside/pages/_app.js
@@ -1,6 +1,21 @@
 import "@/styles/globals.css";
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import {SWRConfig} from 'swr';
+import Layout from "@/components/Layout";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  const fetcher = async (...args)=>{
+    const response = await fetch(...args);
+
+    if(!response.ok){
+      throw new Error(`Request failed with status: ${response.status}`);
+    }
+    return response.json();
+  }
+  return <SWRConfig value={{fetcher}}>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </SWRConfig>;
 }
