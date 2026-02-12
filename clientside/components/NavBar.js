15532a566fe4d8a47eb3ab341d2970e165e1d6f8
Natasha Nicole Ejercito <nnejercito@Natashas-MBP.phub.net.cable.rogers.com>
2026-02-12 14:33:16 -0500

    NavBar.js updated

---
+++ clientside/components/NavBar.js
@@ -0,0 +1,17 @@
+import {Container, Nav, Navbar} from "react-bootstrap"
+import Link from "next/link"
+
+export default function NavBar(){
+    return(
+    <Navbar className="fixed-top navbar-dark bg-dark">
+    <Container>
+        <Navbar.Brand>WikiBooks</Navbar.Brand>
+        <Nav className="me-auto">
+            <Nav.Link as={Link} href="/">Books</Nav.Link>
+            <Nav.Link as={Link} href="/about">About</Nav.Link>
+            
+        </Nav>
+    </Container>
+    </Navbar>
+    )
+}
