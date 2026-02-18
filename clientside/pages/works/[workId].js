de43038a655a6677e6bb3f54938eb86070c2128d
natashaejercito <natasha.ejercito@outlook.com>
2026-02-17 22:09:02 -0500

    workId implemented

---
+++ clientside/pages/works/[workId].js
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router"
+import useSWR from "swr"
+import BookDetails from "@/components/BookDetails"
+import Error from "next/error"
+import PageHeader from "@/components/PageHeader"
+
+export default function Work(){
+    const router = useRouter();
+    const {workId} = router.query;
+
+    const {data, error, isLoading} = useSWR(`https://openlibrary.org/works/${workId}.json`);
+    
+    if(isLoading){
+        return null
+    }
+
+    if (error || !data){
+        return <Error statusCode={404}/>
+    }
+    return(<>
+    <PageHeader text={data.title}/>
+    <BookDetails book={data}/>
+    </>)
+}
