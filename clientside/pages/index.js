dc208c846fc0eab9536d9222821af6d44a80d91f
natashaejercito <natasha.ejercito@outlook.com>
2026-02-17 22:32:13 -0500

    fixed index.js

--- clientside/pages/index.js
+++ clientside/pages/index.js
@@ -5,55 +5,49 @@ import { Pagination, Table } from "react-bootstrap";
 import PageHeader from "@/components/PageHeader";
 export default function Home() {
   const [page, setPage] = useState(1);
-  const [pageData, setPageData] = useState([])
+  const router = useRouter();
+  const author = "Okakura Kakuzo";
 
-  const router = useRouter()
+  const { data } = useSWR(
+    `https://openlibrary.org/search.json?q=author:${encodeURIComponent(author)}&page=${page}&limit=10`
+  );
 
-  const author = "Okakura Kakuzo"
-
-  const {data, error} = useSWR(`https://openlibrary.org/search.json?q=author:${encodeURIComponent(author)}&page=${page}&limit=10`)
-
-  useEffect(()=>{
-    if(data){
-      setPageData(data);
-    }
-  },[data])
-
-  function previous(){
-    if(page > 1){
-      setPage(page-1)
-    }
+  function previous() {
+    if (page > 1) setPage(page - 1);
   }
 
-  function next(){
-    setPage(page + 1)
+  function next() {
+    setPage(page + 1);
   }
+
   return (
     <>
-     <PageHeader text={`Novels by ${author}`}/>
-     <Table stripped hover>
-      <thead>
-        <tr>
-          <th>Title</th>
-          <th>Published</th>
-        </tr>
-      </thead>
-      <tbody>
-        {pageData?.docs?.map((book)=>{
-          <tr key={book.key}
-          onClick={()=> router.push(book.key)}
-          style={{cursor:"pointer"}}>
-            <td>{book.title}</td>
-            <td>{book.first_publish_year ?? "N/A"}</td>
+      <PageHeader text={`Novels by ${author}`} />
+      <Table striped hover>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Published</th>
           </tr>
-        })}
-      </tbody>
-     </Table>
-     <Pagination>
-      <Pagination.Prev onClick={previous}/>
-      <Pagination.Item active>{page}</Pagination.Item>
-      <Pagination.Next onClick={next}/>
-     </Pagination>
+        </thead>
+        <tbody>
+          {data?.docs?.map((book) => (
+            <tr
+              key={book.key}
+              onClick={() => router.push(book.key)}
+              style={{ cursor: "pointer" }}
+            >
+              <td>{book.title}</td>
+              <td>{book.first_publish_year ?? "N/A"}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Pagination>
+        <Pagination.Prev onClick={previous} />
+        <Pagination.Item active>{page}</Pagination.Item>
+        <Pagination.Next onClick={next} />
+      </Pagination>
     </>
   );
 }
